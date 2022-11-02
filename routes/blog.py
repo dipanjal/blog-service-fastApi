8668a6db1d787d9b649767f2f63f9c651263a3e9
dipanjal <dipanjalmaitra@gmail.com>
2022-11-02 11:54:13 +0600

    refactoring

--- routes/blog.py
+++ routes/blog.py
@@ -1,12 +1,11 @@
 from typing import List
-from fastapi import APIRouter, Depends, status, HTTPException
 
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 import database
 import schemas
 from repository import blog_repo
-from schemas import ShowBlog
 
 router = APIRouter(
     prefix="/blogs",
@@ -16,9 +15,14 @@ router = APIRouter(
 get_db = database.get_db
 
 
-@router.get('/', response_model=List[ShowBlog])
-def all(db: Session = Depends(get_db)):
+@router.get('/', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
+def get_all(db: Session = Depends(get_db)):
     return blog_repo.get_all(db)
+
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+def get_by_id(id: int, db: Session = Depends(get_db)):
+    return blog_repo.get_by_id(id, db)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
@@ -27,15 +31,10 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db)):
-    return blog_repo.destroy(id, db)
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    return blog_repo.delete(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     return blog_repo.update(id, request, db)
-
-
-@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
-def show(id: int, db: Session = Depends(get_db)):
-    return blog_repo.show(id, db)
